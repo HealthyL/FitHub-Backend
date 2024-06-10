2500f8cfe3531a2dcae8e4b6f6e466f66e9897ab
BruceACC <u20211d640@upc.edu.pe>
2024-06-10 05:11:31 -0500

    feat:arreglo

---
+++ fithub-backend/Shared/Persistence/Repositories/BaseRepository.cs
@@ -0,0 +1,13 @@
+﻿using BackEnd_FitHub.Shared.Persistence.Contexts;
+
+namespace BackEnd_FitHub.Shared.Persistence.Repositories;
+
+public class BaseRepository
+{
+    protected readonly AppDbContext _context;
+
+    public BaseRepository(AppDbContext context)
+    {
+        _context = context;
+    }
+}
---
+++ fithub-backend/Shared/Persistence/Repositories/UnitOfWork.cs
@@ -0,0 +1,17 @@
+﻿using BackEnd_FitHub.Shared.Persistence.Contexts;
+
+namespace BackEnd_FitHub.Shared.Persistence.Repositories;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly AppDbContext _context;
+    public UnitOfWork(AppDbContext context)
+    {
+        _context = context;
+    }
+
+    public async Task CompleteAsync()
+    {
+        await _context.SaveChangesAsync();
+    }
+}
