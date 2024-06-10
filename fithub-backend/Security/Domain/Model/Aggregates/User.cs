a663245ca9096eb64056b3180a9e5631a69ed284
Anthony07LG <anthony07lg@gmail.com>
2024-06-10 06:59:08 -0500

    feat: Security added

---
+++ fithub-backend/Security/Domain/Model/Aggregates/User.cs
@@ -0,0 +1,23 @@
+﻿using System.Text.Json.Serialization;
+﻿using fithub_backend.Security.Domain.Model.Commands;
+
+namespace fithub_backend.Security.Domain.Model.Aggregates;
+
+/// <summary>
+/// Entidad que representa a un usuario
+/// </summary>
+public class User
+{
+    public int Id { get; set; }
+    public string FullName { get; set; }
+    public string Email { get; set; }
+    [JsonIgnore]
+    public string Password { get; set; }
+    public string Phone { get; set; }
+    public string ImageUrl { get; set; }
+    public string Role { get; set; }
+    
+    //relaciones
+    public IList<Post> Posts { get; set; } = new List<Post>();
+    public IList<Reservation> Reservations { get; set; } = new List<Reservation>();
+}
