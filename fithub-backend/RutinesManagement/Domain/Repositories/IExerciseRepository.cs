5b5faaf73bbb848cd157a2ccb6f63088127c3721
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 17:35:13 -0500

    feat: queries iexerciserepository

---
+++ fithub-backend/RutinesManagement/Domain/Repositories/IExerciseRepository.cs
@@ -0,0 +1,10 @@
+﻿using fithub_backend.RutinesManagement.Domain.Model.Aggregates;
+using fithub_backend.Shared.Domain.Repositories;
+
+namespace fithub_backend.RutinesManagement.Domain.Repositories;
+
+public interface IExerciseRepository:IBaseRepository<Exercise>
+{
+    Task<bool> ExistsByRoutineIdAndExerciseNameAsync(int routineId, string exerciseName);
+    Task<IEnumerable<Exercise>> FindByRoutineIdAsync(int routineId);
+}
