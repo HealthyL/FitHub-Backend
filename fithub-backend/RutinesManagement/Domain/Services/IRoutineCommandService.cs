321c2b59f11c27203881f58da27d144bfde903ee
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 17:41:29 -0500

    feat: services iroutinecommandservice

---
+++ fithub-backend/RutinesManagement/Domain/Services/IRoutineCommandService.cs
@@ -0,0 +1,10 @@
+﻿using fithub_backend.RutinesManagement.Domain.Model.Commands;
+using fithub_backend.RutinesManagement.Domain.Model.Entities;
+
+namespace fithub_backend.RutinesManagement.Domain.Services;
+
+public interface IRoutineCommandService
+{
+    public Task<Routine> Handle(CreateRoutineCommand command);
+
+}
