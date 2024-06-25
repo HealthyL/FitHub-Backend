e2215601fb1235e981b0c626acc9c74c438bcf5e
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 17:44:30 -0500

    feat: repositories rotinerepository

---
+++ fithub-backend/RutinesManagement/Infraestructure/Persistence/EFC/Repositories/RoutineRepository.cs
@@ -0,0 +1,21 @@
+﻿using fithub_backend.RutinesManagement.Domain.Model.Entities;
+using fithub_backend.RutinesManagement.Domain.Repositories;
+using fithub_backend.Shared.Infraestructure.Persistence.EFC.Configuration;
+using fithub_backend.Shared.Infraestructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+
+namespace fithub_backend.RutinesManagement.Infraestructure.Persistence.EFC.Repositories;
+
+public class RoutineRepository (AppDBContext context): BaseRepository<Routine>(context),
+    IRoutineRepository
+{
+    public async Task<bool> ExistsByNameAsync(string Name)
+    {
+        return await Context.Set<Routine>().AnyAsync(routine => routine.Name == Name);
+    }
+    
+    public async Task<bool> ExistsByIdAsync(int RoutineId)
+    {
+        return await Context.Set<Routine>().AnyAsync(routine => routine.Id == RoutineId);
+    }
+}
