5df0c521d14380b4b84a6399eec72e7fc20728df
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 18:05:16 -0500

    fix: program fixed

--- fithub-backend/Program.cs
+++ fithub-backend/Program.cs
@@ -1,13 +1,79 @@
+using fithub_backend.RutinesManagement.Application.Internal.CommandService;
+using fithub_backend.RutinesManagement.Application.Internal.QueryService;
+using fithub_backend.RutinesManagement.Domain.Repositories;
+using fithub_backend.RutinesManagement.Domain.Services;
+using fithub_backend.RutinesManagement.Infraestructure.Persistence.EFC.Repositories;
+using fithub_backend.Shared.Domain.Repositories;
+using fithub_backend.Shared.Infraestructure.Persistence.EFC.Configuration;
+using fithub_backend.Shared.Infraestructure.Persistence.EFC.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<AppDBContext>(
+    options =>
+    {
+        if (connectionString != null)
+            if (builder.Environment.IsDevelopment())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Information)
+                    .EnableSensitiveDataLogging()
+                    .EnableDetailedErrors();
+            else if (builder.Environment.IsProduction())
+                options.UseMySQL(connectionString)
+                    .LogTo(Console.WriteLine, LogLevel.Error)
+                    .EnableDetailedErrors();
+    }
+);
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(
+    c =>
+    {
+        c.SwaggerDoc("v1",
+            new OpenApiInfo
+            {
+                Title="FitHub",
+                Version="v1",
+                Description="FitHub API",
+                TermsOfService=new Uri("https://fithub.com/tos"),
+                Contact=new OpenApiContact
+                {
+                    Name="FitHub Studios",
+                    Email="fithub@klock.com"
+                },
+                License=new OpenApiLicense
+                {
+                    Name = "Apache 2.0",
+                    Url = new Uri("https://www.apache.org/licenses/LICENSE-2.0.html")
+                }
+            });
+        c.EnableAnnotations();
+    }
+);
+builder.Services.AddRouting(options=>options.LowercaseUrls = true);
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
+builder.Services.AddScoped<IExerciseRepository, ExerciseRepository>();
+builder.Services.AddScoped<IExerciseCommandService, ExerciseCommandService>();
+builder.Services.AddScoped<IExerciseQueryService, ExerciseQueryService>();
+builder.Services.AddScoped<IRoutineRepository, RoutineRepository>();
+builder.Services.AddScoped<IRoutineCommandService, RoutineCommandService>();
+builder.Services.AddScoped<IRoutineQueryService, RoutineQueryService>();
+
 
 var app = builder.Build();
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    var context= services.GetRequiredService<AppDBContext>();
+    context.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
