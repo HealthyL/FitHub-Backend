9167c75a64da2b82f513a1064647a384a1784a24
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 17:40:01 -0500

    feat: services iexercisecommandservice

---
+++ fithub-backend/RutinesManagement/Domain/Services/IExerciseCommandService.cs
@@ -0,0 +1,10 @@
+﻿using fithub_backend.RutinesManagement.Domain.Model.Aggregates;
+using fithub_backend.RutinesManagement.Domain.Model.Commands;
+
+namespace fithub_backend.RutinesManagement.Domain.Services;
+
+public interface IExerciseCommandService
+{
+    Task<Exercise?> Handle(CreateExerciseCommand command);
+
+}
