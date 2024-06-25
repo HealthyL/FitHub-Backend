3e6659260e9fa0cf7fe7ba0e3b4c3da87569d3a1
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 17:46:59 -0500

    feat: resources exerciseresource

---
+++ fithub-backend/RutinesManagement/Interfaces/REST/Resources/ExerciseResource.cs
@@ -0,0 +1,3 @@
+﻿namespace fithub_backend.RutinesManagement.Interfaces.REST.Resources;
+
+public record ExerciseResource(int Id, string Name, string PhotoUrl, int Sets, int Reps, string Weight, int RoutineId);
