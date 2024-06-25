cb1cb2ef85b5c6e5eb95f638acc5b0838ed611a2
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 17:28:50 -0500

    feat: aggregates execiseaudit

---
+++ fithub-backend/RutinesManagement/Domain/Model/Aggregates/ExerciseAudit.cs
@@ -0,0 +1,9 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace fithub_backend.RutinesManagement.Domain.Model.Aggregates;
+
+public partial class Exercise
+{
+    [Column("CreatedAt")] public DateTimeOffset? CreatedDate { get; set; }
+    [Column("UpdatedAt")] public DateTimeOffset? UpdatedDate { get; set; }
+}
