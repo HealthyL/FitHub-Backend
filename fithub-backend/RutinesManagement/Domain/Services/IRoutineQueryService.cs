f748e121939284d83b6407f1361d85491d78bc9f
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 17:41:58 -0500

    feat: services irotinequeryservice

---
+++ fithub-backend/RutinesManagement/Domain/Services/IRoutineQueryService.cs
@@ -0,0 +1,11 @@
+﻿using fithub_backend.RutinesManagement.Domain.Model.Commands;
+using fithub_backend.RutinesManagement.Domain.Model.Entities;
+using fithub_backend.RutinesManagement.Domain.Model.Queries;
+
+namespace fithub_backend.RutinesManagement.Domain.Services;
+
+public interface IRoutineQueryService
+{
+    Task<Routine> Handle(GetRoutineByIdQuery query);
+    Task<IEnumerable<Routine>> Handle(GetAllRoutinesQuery query);
+}
