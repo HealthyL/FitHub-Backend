891dca31e39cf4d1a3b2a97f326aaba6ebdc0cef
Anthony07LG <anthony07lg@gmail.com>
2024-06-25 17:07:23 -0500

    fix: aggregates fixed

---
+++ fithub-backend/RutinesManagement/Domain/Model/Aggregates/Exercise.cs
@@ -0,0 +1,26 @@
+﻿using fithub_backend.RutinesManagement.Domain.Model.Entities;
+
+namespace fithub_backend.RutinesManagement.Domain.Model.Aggregates;
+
+public partial class Exercise
+{
+    public int Id { get; private set; }
+    public string Name { get; private set; }
+    public string PhotoUrl { get; private set; }
+    public int Sets { get; private set; }
+    public int Reps { get; private set; }
+    public string Weight { get; private set; }
+    public Routine Routine {get; private set;}
+    public int RoutineId { get; private set; }
+
+    public Exercise(string name, string photoUrl, int sets, int reps, string weight, int routineId)
+    {
+        Name = name;
+        PhotoUrl = photoUrl;
+        Sets = sets;
+        Reps = reps;
+        Weight = weight;
+        RoutineId = routineId;
+    }
+
+}
