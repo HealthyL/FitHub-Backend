e9d533ba1933d29d4fd198a0e6dabd48163496c2
Anthony07LG <anthony07lg@gmail.com>
2024-06-26 08:08:59 -0500

    feat: queryservices exercisequeryservice added

---
+++ fithub-backend/RutinesManagement/Application/Internal/QueryServices/ExerciseQueryService.cs
@@ -0,0 +1,26 @@
+﻿using fithub_backend.RutinesManagement.Domain.Model.Aggregates;
+using fithub_backend.RutinesManagement.Domain.Model.Queries;
+using fithub_backend.RutinesManagement.Domain.Repositories;
+using fithub_backend.RutinesManagement.Domain.Services;
+using fithub_backend.Shared.Domain.Repositories;
+
+namespace fithub_backend.RutinesManagement.Application.Internal.QueryService;
+
+public class ExerciseQueryService(IExerciseRepository exerciseRepository, IUnitOfWork unitOfWork)
+    :IExerciseQueryService
+{
+    public async Task<Exercise?> Handle(GetExerciseByIdQuery query)
+    {
+        return await exerciseRepository.FindByIdAsync(query.Id);
+    }
+
+    public async Task<IEnumerable<Exercise>> Handle(GetAllExercisesQuery query)
+    {
+        return await exerciseRepository.ListAsync();
+    }
+
+    public async Task<IEnumerable<Exercise>> Handle(GetExerciseByRoutineIdQuery query)
+    {
+        return await exerciseRepository.FindByRoutineIdAsync(query.RoutineId);
+    }
+}
